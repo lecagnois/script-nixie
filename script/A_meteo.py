6ef93f4e952e16c421d00e828f5dadde02be4eb4
lecagnois <lecagnois@wanadoo.fr>
2023-02-25 18:19:29 +0100

    modification du 25/02/2023

--- script/A_meteo.py
+++ script/A_meteo.py
@@ -11,22 +11,25 @@ def meteo(town,periode):
         weather.setKey(keymeteo) # cle api du service securite
         weather.setUnits("metric") # or imperial
         weather.setLang("fr") # fr / pour ville en france
-
-    # verification de la cle api
+         
+     # verification de la cle api
     if keymeteo == None:
         i01.speakBlocking(u"Votre clés A P I est périmé ,ou non validé dans le service sécurity")   
     else:
         # l'unité est par pas de 3 heures (1) , de 1 à 40 ( aujourd'hui à 5 jours )
         # récupérer les données brutes pour demain (8) -> parce que 3*8=24H
         # récupérer les données brutes d'aujourd'hui (1) -> demain est 1 :
+        weather.clearLastError()
         weather.setPeriod(periode)
         weather.setLocation(town+",FR")
-        if not str(weather.getWeatherCode())==0:
-            i01_chatBot.getResponse("SYSTEM METEO curtemperature " + str(int(weather.getDegrees())) + " Town " + str(weather.getLocation().split(',')[0]) + " COMMENTAIRE " +  str(weather.getWeatherCode()) + " NBJOUR " + str(periode) + " TEMPMINI " + str(int(weather.getMinDegrees())))
-        else:
+        weather.getWeatherCode()
+        error = weather.getLastError()
+        if error:
             i01_chatBot.getResponse("SYSTEM openweathermapError")
-           
-    
+        else:
+            i01_chatBot.getResponse("SYSTEM METEO curtemperature " + str(int(weather.getDegrees())) + " Town " + str(weather.getLocation().split(',')[0]) + " COMMENTAIRE " +  str(weather.getWeatherCode()) + " NBJOUR " + str(periode))
+              
+     
 # pour mémo service météo
 #print (u"Raw code : " + str(weather.getWeatherCode()))
 #print (u"Ville : " + str(weather.getLocation()))
