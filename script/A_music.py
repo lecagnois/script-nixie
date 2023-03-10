c0bacbdf84ca3702cff4d4bd7b51e92e016d268d
lecagnois <lecagnois@wanadoo.fr>
2023-03-10 11:37:03 +0100

    modification du 10/03/2023

--- script/A_music.py
+++ script/A_music.py
@@ -3,11 +3,10 @@ import os
 
 def chercheplay(morceau):
     if runtime.isStarted('i01.audioPlayer'):
-   
         if os.path.exists('data/config/default/i01.audioPlayer.yml'):
                 global liste
                 global pointeur
-                i01.speakBlocking(u"oké voici le morceau")
+                #i01.speakBlocking(u"oké voici le morceau")
                 f = open('data/config/default/i01.audioPlayer.yml', 'r')
                 liste = f.readlines()
                 #supprime les 11 premiers elements de la liste
@@ -22,7 +21,7 @@ def chercheplay(morceau):
                         i01_audioPlayer.play(var)
                         pointeur = i
                         flag = 1
-                        print(var)
+                        print(var+" Morceau : "+str(pointeur))
                     i=i+1
                 if flag == 0 :
                     i01.speakBlocking(u"Je n'ai pas ce morceau dans mes listes. ou le fichier contient des majuscules.")
@@ -41,9 +40,12 @@ def nextplay():
         var = "".join(liste[pointeur][4:]).rstrip('\n')
         if 'mp3' in var :
             i01_audioPlayer.play(var)
+            print(var+" Morceau : "+str(pointeur))
         else:
-            i01.speakBlocking(u"Je suis arrivé à la fin de la play liste.Je remonte au début de la liste")
-            pointeur = 0
+            i01.speakBlocking(u"Je suis arrivé à la fin de la play liste. Je remonte au début de la liste")
+            pointeur = -1
+            i01_audioPlayer.stop()
+            i01_chatBot.getResponse("SYSTEM MORCEAU SUIVANT")
     else :
         i01.speakBlocking(u"Le service audio player n'es pas lancé. Je le démarre.") 
         AudioPlayer = runtime.start('i01.audioPlayer', 'AudioFile')
@@ -56,10 +58,12 @@ def previousplay():
         var = "".join(liste[pointeur][4:]).rstrip('\n')
         if 'mp3' in var :
             i01_audioPlayer.play(var)
+            print(var+" Morceau : "+str(pointeur))
         else:
-            i01.speakBlocking(u"Je suis arrivé au début de la play liste.Je remonte au début de la liste")
-            pointeur = 0
-            
+            i01.speakBlocking(u"Je suis arrivé au début de la play liste. Je remonte au début de la liste")
+            pointeur = -1
+            i01_audioPlayer.stop()
+            i01_chatBot.getResponse("SYSTEM MORCEAU SUIVANT")
     else :
         i01.speakBlocking(u"Le service audio player n'es pas lancé. Je le démarre.") 
         AudioPlayer = runtime.start('i01.audioPlayer', 'AudioFile')   
