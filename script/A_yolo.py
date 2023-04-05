6cddc2fbbaa3ea09d37b19084e9e5a49df94c5f8
lecagnois <lecagnois@wanadoo.fr>
2023-04-05 18:42:22 +0200

    modification de yolo 05/04/2023

--- script/A_yolo.py
+++ script/A_yolo.py
@@ -1,57 +1,73 @@
-# -- coding: utf-8 --
-last_item_found = None
+## Filtre Yolo 
+# Nous classons les objets par image et obtenons le maximum detecte pour 1 image seulement
+# Ainsi nous pouvons lister et compter en meme temps chaque objet classifie disponible dans le champ de vision, dans un temps donne
+# Obtient egalement la position de l element donne (last_item_found) si defini de tous les autres dans le champ hors vue de gauche a droite
+# classifications est le dictionnaire des objets detectes dans un temps donne
+# apres acquisition et le tri des positions nous lirons le contenu de la collection pour jouer avec
+## avertissement l editeur yolo est maintenant a l interieur de java land pour eviter les problemes de thread a cause du sommeil de python
+# Modification lecagnois  05/04/2023
+global classifications, last_item_found
 
-# données yolo disponibles ici
 def onClassification(data):
+    global last_item_found
+    last_item_found = None
     global classifications, last_item_found
     classifications = data
     for key, value in data.items():
-      print(key)
-      last_item_found = key
-   
-def openyolo():
-    global last_item_found
-    if runtime.isStarted('i01.opencv'):
-        i01_opencv.addFilter("yolo")
-        i01_opencv.setDisplayFilter("yolo")
-        i01_opencv.setCameraIndex(0)
-        i01_opencv.capture()
-        i01_chatBot.getResponse("YOLO_01")
-        classifications = {}
-        last_item_found = None
-        # create a subscription to classification publication
-        python.subscribe('i01.opencv', 'publishClassification')
-     else:
-       i01_chatBot.getResponse("YOLO_04")
-   
-def yolocherche() :
-    global last_item_found
-    if runtime.isStarted('i01.opencv') :
-       #arret du scan  
-       if last_item_found != None :
-          python.unsubscribe('i01.opencv', 'publishClassification')
-          print('dernier objet vu : ', last_item_found)
-          i01_chatBot.getResponse("YOLO_02 " + last_item_found)
-          last_item_found = None
-       else:
-          i01_chatBot.getResponse("YOLO_06")
-    else:
-       i01_chatBot.getResponse("YOLO_05")
-       last_item_found = None
-    #autre classification
-    #python.unsubscribe('i01.opencv', 'publishClassification')
-    #print('confiance ', classifications.get(last_item_found).get(0).getConfidence())
-    #print('position ', classifications.get(last_item_found).get(0).getBoundingBox())
-    
-def closeyolo():
-    global last_item_found
-    i01_chatBot.getResponse("YOLO_03")
-    last_item_found = None
+        #print(key)
+        last_item_found = key
+
+def startYoloInventory(duration):
+  global classifications, last_item_found
+  i01.speakBlocking(i01_chatBot.getPredicate("startupSentence"))
+  #if runtime.isStarted('i01.neoPixel'):
+    #i01_neoPixel.setAnimation("Color Wipe", 25, 5, 10, 15)
+  #Lance function to start & stop yolo filter
+  enableYoloFor(duration)
+  # cherche la valeur dans la class
+  collectionString=""
+  for key, value in classifications.items():
+    collectionString=collectionString+" "+key+", "
+  i01_chatBot.setPredicate("yoloTotalDetected",str(len(classifications)))
+  return collectionString
+
+def getYoloPosition(objet):
+  global classifications, last_item_found
+  i01.speakBlocking(i01_chatBot.getPredicate("startupSentence"))
+  #Lance function to start & stop yolo filter
+  enableYoloFor(6)
+  # cherche la valeur dans la class
+  collectionString=""
+  for key, value in classifications.items():
+    collectionString=collectionString+key+" "
+  #print(unicode(objet, "utf-8"))
+  #print(collectionString) 
+  # position de l objet yolo sauve dans predicate
+  i01_chatBot.setPredicate("yoloTotalDetected",str(len(classifications)))
+  # unicode pour les accents et lower pour la majuscules
+  if (unicode(objet, "utf-8")) in (collectionString.lower()) :
+    i01.speak("j'ai trouver "+(i01_chatBot.getPredicate("article"))+" "+ unicode(objet, "utf-8") + "en position " + str(i01_chatBot.getPredicate("yoloTotalDetected")))
+  else:
+    i01.speak("Je ne vois pas" + (i01_chatBot.getPredicate("article"))+" "+ unicode(objet, "utf-8"))
+
+# function to start & stop yolo filter et tempo classification
+def enableYoloFor(duration):
+  global lastPhotoFileName
+  #start i01.opencv avec filtre yolo
+  if runtime.isStarted('i01.opencv'):
+    i01_opencv.addFilter("yolo")
+    i01_opencv.setDisplayFilter("yolo")
+    i01_opencv.setCameraIndex(0)
+    i01_opencv.capture()
+    classifications = {}
+    # create a subscription to classification publication
+    python.subscribe('i01.opencv', 'publishClassification')
+    # wait for X
+    sleep(duration)
+    lastPhotoFileName = i01_opencv.recordFrame()
+    classifications.clear()
+    python.unsubscribe('i01.opencv', 'publishClassification')
+    i01_opencv.disableFilter("yolo")
     i01_opencv.removeFilter('yolo')
     i01_opencv.stopCapture()
-    i01.cameraOff() 
-    #runtime.release('i01.opencv')
-    
-
-
-    
+    i01.cameraOff()
